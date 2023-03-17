d1e3fb8c0daab48152804cce4b276a38f23cebb3
Gabriel Rodrigues <gabrieloliveira12488@gmail.com>
2023-03-16 23:43:41 -0300

    feat: adding user-data

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group_rule" "http_access" {
   to_port           = 8080
   protocol          = "tcp"
   security_group_id = aws_security_group.sg.id
-  source_security_group_id = aws_security_group.sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 
@@ -42,7 +42,7 @@ resource "aws_security_group_rule" "daemon_port" {
   to_port           = 50000
   protocol          = "tcp"
   security_group_id = aws_security_group.sg.id
-  source_security_group_id = aws_security_group.sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 
@@ -52,7 +52,7 @@ resource "aws_security_group_rule" "ssh_port" {
   to_port           = 22
   protocol          = "tcp"
   security_group_id = aws_security_group.sg.id
-  source_security_group_id = aws_security_group.sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "allow_all" {
@@ -61,19 +61,23 @@ resource "aws_security_group_rule" "allow_all" {
   protocol          = "-1"
   from_port         = 0
   security_group_id = aws_security_group.sg.id
-  source_security_group_id = aws_security_group.sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 
 
-resource "aws_instance" "ecs-server" {
-  ami           = data.aws_ami.docker.id
-  instance_type = "t2.micro"
-  security_groups = [aws_security_group.sg.arn]
+resource "aws_instance" "ecs_server" {
+  ami             = data.aws_ami.docker.id
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.sg.name]
+  key_name        = "jenkins"
 
   tags = {
     Name = "HelloWorld"
   }
 
-  user_data = "#!/bin/bash echo ECS_CLUSTER=your_cluster_name >> /etc/ecs/ecs.config"
+  user_data = <<EOF
+		#! /bin/bash
+    echo ECS_CLUSTER=jenkins-server >> /etc/ecs/ecs.config
+	EOF
 }
