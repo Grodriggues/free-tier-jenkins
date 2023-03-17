a80ade841074c674fe873e08144be2321e0aafe0
Gabriel Rodrigues <gabrieloliveira12488@gmail.com>
2023-03-16 23:06:11 -0300

    feat: fixing sgs

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -22,49 +22,54 @@ resource "aws_iam_instance_profile" "instance_profile" {
   role = data.aws_iam_role.ecs_instance_role.name
 }
 
-resource "aws_security_group" "example" {
-  # ... other configuration ...
-
+resource "aws_security_group" "sg" {
   name = "jenkins-sg"
-
-  ingress {
-    description = "Allows TCP on 8080"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-  }
-
-
-  ingress {
-    description = "Allows TCP on 8080"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-  }
-
-
-
-  ingress {
-    description = "Allows TCP on 50000"
-    from_port   = 50000
-    to_port     = 50000
-    protocol    = "tcp"
-
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
 }
+
+resource "aws_security_group_rule" "http_access" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  security_group_id = aws_security_group.sg.id
+  source_security_group_id = aws_security_group.sg.id
+}
+
+
+resource "aws_security_group_rule" "daemon_port" {
+  type              = "ingress"
+  from_port         = 50000
+  to_port           = 50000
+  protocol          = "tcp"
+  security_group_id = aws_security_group.sg.id
+  source_security_group_id = aws_security_group.sg.id
+}
+
+
+resource "aws_security_group_rule" "ssh_port" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.sg.id
+  source_security_group_id = aws_security_group.sg.id
+}
+
+resource "aws_security_group_rule" "allow_all" {
+  type              = "egress"
+  to_port           = 0
+  protocol          = "-1"
+  from_port         = 0
+  security_group_id = aws_security_group.sg.id
+  source_security_group_id = aws_security_group.sg.id
+}
+
 
 
 resource "aws_instance" "ecs-server" {
   ami           = data.aws_ami.docker.id
   instance_type = "t2.micro"
+  security_groups = [aws_security_group.sg.arn]
 
   tags = {
     Name = "HelloWorld"
